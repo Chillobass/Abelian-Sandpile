b4ea642a3562c3403516cebd9b25ca82577b6c79
Mikael Burunsuzian <106838313+Chillobass@users.noreply.github.com>
2025-03-06 13:24:45 +0300

    Create parser.h

---
+++ parser.h
@@ -0,0 +1,9 @@
+#ifndef PARSER_H
+#define PARSER_H
+#include <iostream>
+#include <cstring>
+#pragma once
+
+void parse_arguments(int* argc, char** &argv , char* &input_file, char* &output_file, int* maxiter, int* freq);
+
+#endif
